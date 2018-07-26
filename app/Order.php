76a00de8d19613210b2a6f82deecebcf221e4606
kuzma <v.kuzma@mail.ru>
2018-07-27 00:53:08 +0300

    DB save printer_order

--- app/Order.php
+++ app/Order.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use DB;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
 use Request;
@@ -47,15 +48,53 @@ class Order extends Model
         return Status::all();
     }
 
-    public function printer(){
-        return $this->belongsTo(Printer::class);
-    }
+   // public function printers(){
+  //      return $this->belongsToMany(Printer::class);
+   // }
 
-    public function cartridge(){
-        return $this->belongsTo(Cartridge::class);
-    }
+  //  public function cartridge(){
+   //     return $this->belongsTo(Cartridge::class);
+  //  }
 
     public function routeNotificationForMail(){
         return $this->contractor->email;
+    }
+
+    public function addPrinterOrder($request, $order_id){
+        $fields = [];
+        foreach ($request->printer as $printer){
+            if($request->cartridge){
+                foreach ($request->cartridge[$printer] as $key=>$val){
+                    $fields[] = [
+                        'order_id' => $order_id,
+                        'printer_id' => $printer,
+                        'cartridge_id' => $val,
+                        'quantity' => $request->count_cartridge[$printer][$key]
+                    ];
+                }
+            }else{
+                $fields[] = [
+                    'order_id' => $order_id,
+                    'printer_id' => $printer,
+                    'cartridge_id' => 0,
+                    'quantity' => 0
+                ];
+            }
+        };
+
+        DB::table('order_printer')->insert($fields);
+    }
+
+    public function updatePrinterOrder($request, $order_id){
+        //
+    }
+
+    public function showPrinterOrder($order_id){
+        $printers = DB::table('order_printer')->where('options->language', 'en')->get();
+        return $printers;
+    }
+
+    public function delPrinterOrder($order_id){
+        DB::table('order_printer')->where('order_id', $order_id)->delete();
     }
 }
