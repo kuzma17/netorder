99088859a91428a4a45945836705b9dc5c0422d7
kuzma <v.kuzma@mail.ru>
2018-07-28 12:51:02 +0300

    Revert "Revert "Revert "DB save printer_order"""
    This reverts commit 9450528c238768f86dff9fc943c3603215f8081d.

--- resources/views/order/select_cartridge.blade.php
+++ resources/views/order/select_cartridge.blade.php
@@ -1,7 +1,7 @@
 <div class="row cartridge">
     <label class="col-md-3 control-label">Картридж/Количество</label>
     <div class="col-md-6">
-        <select name="cartridge[{{$printer_id}}][]" class="form-control select_order_cartridge" required>
+        <select name="cartridge[]" class="form-control select_order_cartridge" required>
 
                <option value="">Выберите картридж</option>
 
@@ -14,7 +14,7 @@
         </select>
      </div>
     <div class="col-md-2">
-        <input class="form-control" type="text" name="count_cartridge[{{$printer_id}}][]" value="1" required>
+        <input class="form-control" type="text" name="count_cartridge[]" value="1" required>
     </div>
     <div class="col-md-1">
         <a href="#" class="order_del_cartridge" title="Удалить картридж">
